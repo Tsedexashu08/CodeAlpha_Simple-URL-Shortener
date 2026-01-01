df53d8f33ab470107e314dc039190d528382e621
Tsedexashu08 <ashutsedex@gmail.com>
2026-01-01 20:18:05 +0300

    added the url shortening logic after  saving to the database

--- public/server.js
+++ public/server.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-const supabase = require('./supabaseClient');
+const supabase = require("./supabaseClient");
 const PORT = process.env.PORT || 3000;
+const shortId = require("shortid");
 
-app.use(express.static(path.join(__dirname, 'public')));
-const page = path.join(__dirname, 'index.html');
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.sendFile(page);
-    supabase.from('urls').select('*').then(({data, error}) => {
-        if (error) {
-            console.error('Error fetching URLs:', error);
-        } else {
-            console.log('Fetched URLs:', data);
-        }
-  });
+app.use(express.static(path.join(__dirname, "public")));
+const page = path.join(__dirname, "index.html");
+
+app.get("/", (req, res) => {
+  res.sendFile(page);
 });
-
+app.post("/shortenURL", async (req, res) => {
+  await supabase
+    .from("urls")
+    .insert({ URL: req.body.url, ShortenedUrl: shortId.generate() });
+});
 app.listen(PORT, () => {
-    console.log(`Server listening on port : ${PORT}`)
+  console.log(`Server listening on port : ${PORT}`);
 });
--- public/supabaseClient.js
+++ public/supabaseClient.js
@@ -5,9 +5,6 @@ const {createClient} = require('@supabase/supabase-js');
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_ANON_KEY;
 
-console.log('Supabase URL:', supabaseUrl ? 'Set' : 'NOT SET');
-console.log('Supabase Key:', supabaseKey ? 'Set' : 'NOT SET');
-
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
 
 module.exports = supabase;
